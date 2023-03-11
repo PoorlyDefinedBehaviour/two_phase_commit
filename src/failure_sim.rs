37124169eebd98012a4fff931619ab5454d938f4
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-03-11 00:48:48 -0300

    fix(failure_sim): failure chance was too high, change it to 10%

--- src/failure_sim.rs
+++ src/failure_sim.rs
@@ -2,22 +2,22 @@
 
 use rand::Rng;
 
-pub fn participant_should_be_able_to_commit() -> bool {
-    rand::thread_rng().gen_bool(0.9)
+pub fn participant_should_not_be_able_to_commit() -> bool {
+    rand::thread_rng().gen_bool(0.1)
 }
 
 pub fn prepare_commit_should_fail() -> bool {
-    rand::thread_rng().gen_bool(0.9)
+    rand::thread_rng().gen_bool(0.1)
 }
 
 pub fn commit_should_fail() -> bool {
-    rand::thread_rng().gen_bool(0.9)
+    rand::thread_rng().gen_bool(0.1)
 }
 
 pub fn abort_should_fail() -> bool {
-    rand::thread_rng().gen_bool(0.9)
+    rand::thread_rng().gen_bool(0.1)
 }
 
 pub fn query_transaction_state_should_fail() -> bool {
-    rand::thread_rng().gen_bool(0.9)
+    rand::thread_rng().gen_bool(0.1)
 }
